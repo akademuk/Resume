3309c688bff16d9c36b20d90821dddeb70b45c2b
Oleh Fedorov <akademuk24@gmail.com>
2020-03-29 16:31:35 +0300

    ipad_test

--- js/progress_bar.js
+++ js/progress_bar.js
@@ -1,12 +1,12 @@
 var lang = {
   html: "100%",
   css: "90%",
-  javascript: "70%",
+  javascript: "25%",
   php: "55%",
   bootstrap: "65%",
   responsive: "65%",
-  photoshop: "65%",
-  wordpress: "65%"
+  photoshop: "35%",
+  wordpress: "35%"
 };
 
 var multiply = 4;
